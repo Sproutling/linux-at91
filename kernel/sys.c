045b4fce314e783cb93fff7df4d9fae45fb00f90
Sophia Wang <sophia@sproutling.com>
2015-08-07 15:34:13 -0700

    reboot fix

--- kernel/sys.c
+++ kernel/sys.c
@@ -395,15 +395,7 @@ static void migrate_to_reboot_cpu(void)
  */
 void kernel_restart(char *cmd)
 {
-	kernel_restart_prepare(cmd);
-	migrate_to_reboot_cpu();
-	syscore_shutdown();
-	if (!cmd)
-		printk(KERN_EMERG "Restarting system.\n");
-	else
-		printk(KERN_EMERG "Restarting system with command '%s'.\n", cmd);
-	kmsg_dump(KMSG_DUMP_RESTART);
-	machine_restart(cmd);
+	kernel_power_off();
 }
 EXPORT_SYMBOL_GPL(kernel_restart);
 
